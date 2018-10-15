3206d3a795206f470429d7c1c203d7fea86b8ea1
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 02:49:43 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,28 +1,28 @@
-CREATE TABLE series (
+CREATE TABLE Series (
 id INTEGER PRIMARY KEY,
 title TEXT,
 author_id INTEGER,
 subgenre_id INTEGER
 );
 
-CREATE TABLE subgenres (
+CREATE TABLE Subgenres (
 id INTEGER PRIMARY KEY,
 name TEXT
 );
 
-CREATE TABLE authors (
+CREATE TABLE Authors (
 id INTEGER PRIMARY KEY,
 name TEXT
 );
 
-CREATE TABLE books (
+CREATE TABLE Books (
 id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER,
 series_id INTEGER
 );
 
-CREATE characters (
+CREATE Characters (
 id INTEGER PRIMARY KEY,
 name TEXT,
 motto TEXT,
