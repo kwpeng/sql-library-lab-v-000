32a7eecc818f8b71345622366817f12bea8ba07c
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 03:01:31 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,8 +13,7 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, SUM(characters.species) FROM characters
-  HAVING M
-  GROUP BY SUM(books.id) LIMIT 1"
+  HAVING MAX(SUM(characters.species))"
 end
 
 def select_name_and_series_subgenres_of_authors
