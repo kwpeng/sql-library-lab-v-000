4c74ba91b8ebee5aedfd8f659278f9774bf7d1e3
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 01:51:28 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  SELECT name, series.title, subgenres.name FROM authors
+  SELECT authors.name, series.title, subgenres.name FROM subgenres
   JOIN 
 end
 
