f7155b19e9e0cdbaf4de26eb61495dfeadf1afb1
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 02:21:39 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,9 +22,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  SELECT series.title FROM series 
+  SELECT series.title, SUM(series.id) FROM series 
   JOIN characters WHERE series.id = characters_series.id
-  GROUP BY characters.species 
+  GROUP BY characters.species HAVING 
   
 end
 
