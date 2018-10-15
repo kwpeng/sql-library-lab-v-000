490278906eeed6bec4f455b032f173e26a734c8b
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 02:04:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,8 @@ end
 
 def select_series_title_with_most_human_characters
   SELECT series.title FROM series 
-  JOIN books WHERE 
+  JOIN books WHERE series.id = books_series.id
+  
 end
 
 def select_character_names_and_number_of_books_they_are_in
