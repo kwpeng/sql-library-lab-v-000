9191ce076891ed89dbbfa15055b4f627caff18fd
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 03:02:17 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, SUM(characters.species) FROM characters
+  "SELECT characters.species, COUNT(characters.species) FROM characters
   HAVING MAX(SUM(characters.species))"
 end
 
