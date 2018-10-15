5739abd7e1aa14d67d7eb5134bfae28231be1f7f
bluebeadlily <bluebeadlily@gmail.com>
2018-10-15 03:03:52 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -13,7 +13,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) FROM characters
-  HAVING MAX(COUNT(characters.species))"
+  GROUP BY species
+  ORDER BY COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
